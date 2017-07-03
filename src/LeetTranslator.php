f4f30645bbfcf4862207f25e9f0d4ecf7461eca5
jacob ruleaux and Steve Spitz <jacob@>
2017-07-03 16:17:00 -0700

    debugging final test

--- src/LeetTranslator.php
+++ src/LeetTranslator.php
@@ -3,9 +3,60 @@ class LeetSpeakTranslator
 {
 
 
-    function leetMaker()
+    function leetMaker($input_string)
     {
-
+        $output_array = array();
+        $output_word = array();
+        $word_array = explode(" ", $input_string);
+        foreach ($word_array as $word)
+        {
+            $pos = strpos($word, "S")
+            if ($pos !== false)
+            {
+                array_push($output_word, substr_replace($word, "&", $pos, strlen("S"));
+            }
+            
+            if ($word[0] == "S")
+            {
+                array_push($output_word, str_replace("S","&", $word));
+            } elseif ($word[0] == "s")
+            {
+                array_push($output_word, str_replace("s","*", $word));
+            } else
+            {
+                array_push($output_word, $word);
+            }
+        }
+        $feeder_input = implode(" ", $output_word);
+        $input_array = str_split($feeder_input);
+        foreach($input_array as $letter)
+        {
+            if ($letter == "I") {
+                array_push($output_array, "1");
+            } elseif ($letter == "O" || $letter == "o")
+            {
+                array_push($output_array, "0");
+            } elseif ($letter == "E" || $letter == "e")
+            {
+                array_push($output_array, "3");
+            } elseif ($letter == "S")
+            {
+                array_push($output_array, "Z");
+            } elseif ($letter == "s")
+            {
+                array_push($output_array, "z");
+            } elseif ($letter == "&")
+            {
+                array_push($output_array, "S");
+            } elseif ($letter == "*")
+            {
+                array_push($output_array, "s");
+            } else
+            {
+                array_push($output_array, $letter);
+            }
+        }
+        return implode($output_array);
     }
 }
 ?>
--- tests/leetTranslatorTest.php
+++ tests/leetTranslatorTest.php
@@ -5,7 +5,7 @@
     class LeetSpeakTranslatorTest extends PHPUnit_Framework_TestCase
     {
 
-        function test_leetSpeak_oneWord()
+        function test_leetSpeak_oneI()
         {
             //Arrange
             $test_LeetSpeakTranslator = new LeetSpeakTranslator;
@@ -16,6 +16,42 @@
 
             //Assert
             $this->assertEquals("1", $result);
+        }
+        function test_leetSpeak_twoO()
+        {
+            //Arrange
+            $test_LeetSpeakTranslator = new LeetSpeakTranslator;
+            $input = "Oo";
+
+            //Act
+            $result = $test_LeetSpeakTranslator->leetMaker($input);
+
+            //Assert
+            $this->assertEquals("00", $result);
+        }
+        function test_leetSpeak_twoE()
+        {
+            //Arrange
+            $test_LeetSpeakTranslator = new LeetSpeakTranslator;
+            $input = "Ee";
+
+            //Act
+            $result = $test_LeetSpeakTranslator->leetMaker($input);
+
+            //Assert
+            $this->assertEquals("33", $result);
+        }
+        function test_leetSpeak_eStuary()
+        {
+            //Arrange
+            $test_LeetSpeakTranslator = new LeetSpeakTranslator;
+            $input = "Speak to snarsgotShs";
+
+            //Act
+            $result = $test_LeetSpeakTranslator->leetMaker($input);
+
+            //Assert
+            $this->assertEquals("Sp3ak t0 snarzg0tZhz", $result);
         }
     }
 
