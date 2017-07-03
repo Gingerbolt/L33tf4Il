c6de92a2a19ec518a74aae2bf852e5fd76ce7509
jacob ruleaux and Steve Spitz <jacob@>
2017-07-03 16:51:12 -0700

    Add full refactor to try to meet final spec.

--- src/LeetTranslator.php
+++ src/LeetTranslator.php
@@ -10,51 +10,55 @@ class LeetSpeakTranslator
         $word_array = explode(" ", $input_string);
         foreach ($word_array as $word)
         {
-            $pos = strpos($word, "S")
-            if ($pos !== false)
+            $query_array = ["I", "E", "e", "O", "o"];
+            $replacement_array = ["1", "3", "3", "0", "0"];
+            array_push($output_word, str_replace($query_array,$replacement_array, $word));
+        }
+        foreach ($output_word as $werd)
+        {
+            if ($werd[0] === "S" || $werd[0] === "s")
             {
-                array_push($output_word, substr_replace($word, "&", $pos, strlen("S"));
-            }
-            
-            if ($word[0] == "S")
-            {
-                array_push($output_word, str_replace("S","&", $word));
-            } elseif ($word[0] == "s")
-            {
-                array_push($output_word, str_replace("s","*", $word));
-            } else
-            {
-                array_push($output_word, $word);
+                for ($x = 1; $x < strlen($werd); ++$x)
+                {
+                    if ($werd[$x] === "s")
+                    {
+                        $werd[$x] = "z"
+                    }
+                    if ($werd[$x] === "S")
+                    {
+                        $werd[$x] = "Z"
+                    }
+                }
             }
         }
-        $feeder_input = implode(" ", $output_word);
-        $input_array = str_split($feeder_input);
-        foreach($input_array as $letter)
-        {
-            if ($letter == "I") {
-                array_push($output_array, "1");
-            } elseif ($letter == "O" || $letter == "o")
-            {
-                array_push($output_array, "0");
-            } elseif ($letter == "E" || $letter == "e")
-            {
-                array_push($output_array, "3");
-            } elseif ($letter == "S")
-            {
-                array_push($output_array, "Z");
-            } elseif ($letter == "s")
-            {
-                array_push($output_array, "z");
-            } elseif ($letter == "&")
-            {
-                array_push($output_array, "S");
-            } elseif ($letter == "*")
-            {
-                array_push($output_array, "s");
-            } else
-            {
-                array_push($output_array, $letter);
-            }
+        // $feeder_input = implode(" ", $output_word);
+        // $input_array = str_split($feeder_input);
+        // foreach($input_array as $letter)
+        // {
+        //     if ($letter == "I") {
+        //         array_push($output_array, "1");
+        //     } elseif ($letter == "O" || $letter == "o")
+        //     {
+        //         array_push($output_array, "0");
+        //     } elseif ($letter == "E" || $letter == "e")
+        //     {
+        //         array_push($output_array, "3");
+        //     } elseif ($letter == "S")
+        //     {
+        //         array_push($output_array, "Z");
+        //     } elseif ($letter == "s")
+        //     {
+        //         array_push($output_array, "z");
+        //     } elseif ($letter == "&")
+        //     {
+        //         array_push($output_array, "S");
+        //     } elseif ($letter == "*")
+        //     {
+        //         array_push($output_array, "s");
+        //     } else
+        //     {
+        //         array_push($output_array, $letter);
+        //     }
         }
         return implode($output_array);
     }
